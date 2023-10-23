66a9a3adec749fb9a1b57d4f5047ea33f66b7e66
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-23 20:49:36 +0100

    add weather app

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <p>
+      This project was coded by Fernanda Gomes and is open-sourced on{" "}
+      <a href="" target="_blank" rel="noopener noreferrer">
+        GitHub
+      </a>{" "}
+      and hosted on{" "}
+      <a href="" target="_blank" rel="noopener noreferrer">
+        Netlify
+      </a>
+    </p>
+  );
+}
