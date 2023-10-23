9976c0460b05a5a2183740c2ffb814b5620f5578
FernandaSGomes <nandasthefany@hotmail.com>
2023-10-23 20:58:15 +0100

    add Github link

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,12 @@ import React from "react";
 export default function Footer() {
   return (
     <p>
-      This project was coded by Fernanda Gomes and is open-sourced on{" "}
+      This project was coded by Fernanda Gomes and is open-sourced on
+      {"https://github.com/FernandaSGomes/weather-react"}
       <a href="" target="_blank" rel="noopener noreferrer">
         GitHub
-      </a>{" "}
+      </a>
+      {"https://github.com/FernandaSGomes/weather-react"}
       and hosted on{" "}
       <a href="" target="_blank" rel="noopener noreferrer">
         Netlify
